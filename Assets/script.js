e550ed064388b5a64173c49361ab04ff089ca0a7
Steph Heins <stephanie_heins@yahoo.com>
2021-10-11 20:45:03 -0700

    Cleaned up JS notes

--- Assets/script.js
+++ Assets/script.js
@@ -107,13 +107,9 @@ function renderScore(){
     const finalScore = Math.round(100 * scoreCount/questions.length);
 
     userScore.innerHTML = finalScore + "%"; 
-
-
-
 }
 
 // Past Scores
-
 var initialInput = document.querySelector("#initial-text");
 var initialForm = document.querySelector("#initial-form");
 var initialList = document.querySelector("#initial-list");
@@ -121,13 +117,10 @@ var initialCountSpan = document.querySelector("#initial-count");
 
 var pastScores = [];
 
-// The following function renders items in a todo list as <li> elements
 function renderPastScores() {
-  // Clear todoList element and update todoCountSpan
   initialList.innerHTML = "";
   initialCountSpan.textContent = pastScores.length;
 
-  // Render a new li for each todo
   for (var i = 0; i < pastScores.length; i++) {
     var pastScore = pastScores[i];
 
@@ -143,60 +136,46 @@ function renderPastScores() {
   }
 }
 
-// This function is being called below and will run when the page loads.
 function init() {
-  // Get stored todos from localStorage
   var storedScores = JSON.parse(localStorage.getItem("pastScores"));
 
-  // If todos were retrieved from localStorage, update the todos array to it
   if (storedScores !== null) {
     pastScores = storedScores;
   }
 
-  // This is a helper function that will render todos to the DOM
   renderPastScores();
 }
 
 function storeScores() {
-  // Stringify and set key in localStorage to todos array
   localStorage.setItem("pastScores", JSON.stringify(pastScores));
 }
 
-// Add submit event to form
 initialForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
   var initialText = initialInput.value.trim();
 
-  // Return from function early if submitted todoText is blank
   if (initialText === "") {
     return;
   }
 
-  // Add new todoText to todos array, clear the input
   pastScores.push(initialText);
   initialInput.value = "";
 
-  // Store updated todos in localStorage, re-render the list
   storeScores();
   renderPastScores();
 });
 
-// Add click event to todoList element
 initialList.addEventListener("click", function(event) {
   var element = event.target;
 
-  // Checks if element is a button
   if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
     var index = element.parentElement.getAttribute("data-index");
     pastScores.splice(index, 1);
 
-    // Store updated todos in localStorage, re-render the list
     storeScores();
     renderPastScores();
   }
 });
 
-// Calls init to retrieve data and render it to the page on load
 init()
